31d0d13eddb8173aa0e0dbd7e4cee6b47674f461
Ömer Faruk Kurt <kurtomerfaruk@gmail.com>
2024-07-14 13:19:46 +0300

    ManyToOne added

--- src/main/java/com/kurtomerfaruk/martfury/models/Faqs.java
+++ src/main/java/com/kurtomerfaruk/martfury/models/Faqs.java
@@ -1,9 +1,9 @@
 package com.kurtomerfaruk.martfury.models;
 
-import jakarta.persistence.Basic;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
 import lombok.*;
+
+import java.io.Serial;
 
 /**
  * @author Omer Faruk KURT kurtomerfaruk@gmail.com
@@ -11,21 +11,24 @@ import lombok.*;
  * @since 14.07.2024 11:22
  */
 @Entity
+@Table(name = "faqs", schema = "martfury", catalog = "")
 @Builder
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class Faqs extends BaseEntity<Long>{
+    @Serial
+    private static final long serialVersionUID = -2942174591740525825L;
     @Basic
     @Column(name = "question")
     private String question;
     @Basic
     @Column(name = "answer")
     private String answer;
-    @Basic
-    @Column(name = "category_id")
-    private Object categoryId;
+    @JoinColumn(name = "category_id", referencedColumnName = "id")
+    @ManyToOne(fetch = FetchType.EAGER)
+    private FaqCategories category;
     @Basic
     @Column(name = "status")
     private String status;
